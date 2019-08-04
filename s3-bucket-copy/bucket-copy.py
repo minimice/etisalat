08b4a6c3f52db486b8278b8bc1997feb75949b6d
Chooi-Guan Lim <skyjuicesoftware@gmail.com>
2019-08-04 15:49:50 +0400

    Initial script to copy files to an s3 bucket

---
+++ s3-bucket-copy/bucket-copy.py
@@ -0,0 +1,17 @@
+import boto3
+import os
+import json
+import datetime
+
+#TODO Create Lambda IAM role
+#TODO Create Lambda
+#TODO Deploy Lambda
+#TODO Create cron job to run lambda periodically
+
+# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3.Client.upload_file
+def main():
+    s3 = boto3.resource('s3')
+    s3.meta.client.upload_file('/tmp/hello.txt', 'mybucket', 'hello.txt')
+    
+if __name__== "__main__":
+    main()
