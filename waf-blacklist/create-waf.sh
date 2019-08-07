caf6f7afd360d5c72e71718f1dddb146dfc66d76
Chooi-Guan Lim <skyjuicesoftware@gmail.com>
2019-08-07 10:26:56 +0400

    Script to create WAF rules

---
+++ waf-blacklist/create-waf.sh
@@ -0,0 +1,28 @@
+
+#!/bin/bash
+
+# Read blacklisted-ips.txt
+# Loop through line by line
+# Store each IP in the object
+# 
+# ip_set_descriptor {
+#   type  = "IPV4"
+#   value = ""
+# }
+#
+# Save the object in a file 
+
+
+# dev env
+input="./blacklisted-ips.txt"
+
+while IFS= read -r line
+do
+  echo 'ip_set_descriptor {' >> result.txt
+  echo '  type  = "IPV4"' >> result.txt
+  echo '  vale  = "'${line}'/32"' >> result.txt
+  echo '}' >> result.txt
+  echo ' ' >> result.txt
+done < "$input"
+
+echo "Done"
