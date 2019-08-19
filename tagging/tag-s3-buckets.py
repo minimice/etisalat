d45b2749d90672432746a1a32954db6b820ead6c
Chooi-Guan Lim <skyjuicesoftware@gmail.com>
2019-08-19 13:09:25 +0400

    Tag s3 buckets

---
+++ tagging/tag-s3-buckets.py
@@ -0,0 +1,36 @@
+import boto3
+from botocore.exceptions import ClientError
+
+# Set your environment variables before running this script.  The following values are fake, use real values when running this
+# i.e.
+# set AWS_ACCESS_KEY_ID=AKIA6D4T4XZGSR7S1234
+# set AWS_SECRET_ACCESS_KEY=II9hASEn6jJENpdDErCI1C+9VX0EGKMXEkEx1234
+# set AWS_DEFAULT_REGION=eu-west-1
+
+key = 'SampleKey'
+value = 'SampleValue'
+
+s3 = boto3.resource('s3')
+client = boto3.client('s3')
+
+# Loop through all buckets
+buckets = []
+response = client.list_buckets()
+for bucket in response['Buckets']:
+    #print(bucket['Name'])
+    buckets.append(bucket['Name'])
+
+# For each bucket, add the tag
+for bucket in buckets:
+    # print(bucket)    
+    bucket_tagging = s3.BucketTagging(bucket)
+    tags = []
+    # Try to get existing tags
+    try:
+        tags = bucket_tagging.tag_set
+    except ClientError as e:
+        print("Caught unexpected error: %s" % e)
+    tags.append({'Key': key, 'Value': value})
+    # print(tags)
+    # Tag bucket with newly appended tag
+    Set_Tag = bucket_tagging.put(Tagging={'TagSet':tags})
