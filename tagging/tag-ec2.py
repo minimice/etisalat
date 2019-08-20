c23014540652074f30d6708f57643fe7d724b03f
Chooi-Guan Lim <skyjuicesoftware@gmail.com>
2019-08-20 10:27:43 +0400

    Now supports profiles for easier mass tagging

--- tagging/tag-ec2.py
+++ tagging/tag-ec2.py
@@ -3,25 +3,46 @@ import json
 from botocore.exceptions import ClientError
 
 def lambda_handler(event, context):
-    # If running from the command line, set your environment variables.
-    # i.e. AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY and AWS_DEFAULT_REGION
 
-    # EC2 tag (Name) to search for, i.e. machines which you want to tag 
-    # Use '*' to tag ALL EC2 instances
-    # i.e. tagValue = '*'
-    matchingTagNameValue = 'Dev Portal Bastion'
+    # Load profiles from credentials file located at ~/.aws/credentials
+    # e.g. dev, prod, test, etc.
+    devSession = boto3.Session(profile_name='dev')
+    devClient = devSession.client('ec2')
+
+    testSession = boto3.Session(profile_name='test')
+    testClient = testSession.client('ec2')
+
+    prodSession = boto3.Session(profile_name='prod')
+    prodClient = prodSession.client('ec2')
+
+    # Set matchingTagName '*' to tag ALL EC2 instances
+    # i.e. matchingTagName = '*' or 
+    #      matchingTagName = 'Dev Portal Bastion' to match only EC2s with the name tag of 'Dev Portal Bastion'
     # The tag you want to add, e.g. Environment tag with value Development
-    newTagName = 'NewTagName'
-    newTagValue = 'NewTagValue'
-    
-    client = boto3.client('ec2')
+    #
+    # Set newTagName to the tag you want to add or update
+    # e.g. newTagName = 'Environment' where 'Environment' is the tag you want to add or update
+    #
+    # Set newTagValue to the value of the tag
+    # e.g. newTagValue = 'Dev' where 'Environment' will have a value of 'Dev'    
 
-    # Initial request, search for all EC2s which match tag (Name)
-    reservations = search_and_tag(matchingTagNameValue, newTagName, newTagValue, client)
-    
-    # Paginate if there are additional results and tag
-    while 'NextToken' in reservations:
-        reservations = search_and_tag(matchingTagNameValue, newTagName, newTagValue, client, reservations['NextToken'])
+    clients = [{'client': devClient,  'matchingTagName': '*', 'newTagName': 'Environment', 'newTagValue': 'Dev'},
+               {'client': testClient, 'matchingTagName': '*', 'newTagName': 'Environment', 'newTagValue': 'Test'},
+               {'client': prodClient, 'matchingTagName': '*', 'newTagName': 'Environment', 'newTagValue': 'Prod'}]
+
+    for clientAndTag in clients:
+
+        client = clientAndTag['client']
+        matchingTagName = clientAndTag['matchingTagName']
+        tagName = clientAndTag['newTagName']
+        tagValue = clientAndTag['newTagValue']
+
+        # Initial request, search for all EC2s which match tag (Name)
+        reservations = search_and_tag(matchingTagName, tagName, tagValue, client)
+        
+        # Paginate if there are additional results and tag
+        while 'NextToken' in reservations:
+            reservations = search_and_tag(matchingTagName, tagName, tagValue, client, reservations['NextToken'])
 
     return {
         'statusCode': 200,
